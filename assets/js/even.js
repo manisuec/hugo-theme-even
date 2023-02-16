e73e9609c965713defaf4d4790489b44f3a156ef
Manish Prasad <manishvaishu25@gmail.com>
2023-02-16 15:53:57 +0530

    updated javascript

--- assets/js/even.js
+++ assets/js/even.js
@@ -21,31 +21,31 @@ Even.backToTop = function() {
 Even.mobileNavbar = function() {
   const $mobileNav = $('#mobile-navbar');
   const $mobileNavIcon = $('.mobile-navbar-icon');
-  const slideout = new Slideout({
-    'panel': document.getElementById('mobile-panel'),
-    'menu': document.getElementById('mobile-menu'),
-    'padding': 180,
-    'tolerance': 70,
-  });
-  slideout.disableTouch();
+  // const slideout = new Slideout({
+  //   'panel': document.getElementById('mobile-panel'),
+  //   'menu': document.getElementById('mobile-menu'),
+  //   'padding': 180,
+  //   'tolerance': 70,
+  // });
+  // slideout.disableTouch();
 
-  $mobileNavIcon.click(function() {
-    slideout.toggle();
-  });
+  // $mobileNavIcon.click(function() {
+  //   slideout.toggle();
+  // });
 
-  slideout.on('beforeopen', function() {
-    $mobileNav.addClass('fixed-open');
-    $mobileNavIcon.addClass('icon-click').removeClass('icon-out');
-  });
+  // slideout.on('beforeopen', function() {
+  //   $mobileNav.addClass('fixed-open');
+  //   $mobileNavIcon.addClass('icon-click').removeClass('icon-out');
+  // });
 
-  slideout.on('beforeclose', function() {
-    $mobileNav.removeClass('fixed-open');
-    $mobileNavIcon.addClass('icon-out').removeClass('icon-click');
-  });
+  // slideout.on('beforeclose', function() {
+  //   $mobileNav.removeClass('fixed-open');
+  //   $mobileNavIcon.addClass('icon-out').removeClass('icon-click');
+  // });
 
-  $('#mobile-panel').on('touchend', function() {
-    slideout.isOpen() && $mobileNavIcon.click();
-  });
+  // $('#mobile-panel').on('touchend', function() {
+  //   slideout.isOpen() && $mobileNavIcon.click();
+  // });
 };
 
 Even._initToc = function() {
